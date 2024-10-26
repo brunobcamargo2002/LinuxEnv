c6dc3e958bbc3ed38546851e1cb6c73710a9ceff
bruno <bruno567bruno@gmail.com>
2024-10-26 11:46:25 -0300

    a

--- Apps.sh
+++ Apps.sh
@@ -1,6 +1,9 @@
 #Basic
 
 PKG=snap
+PKG2=apt
 INST=install
-sudo $PKG $INST nvim tmux calcurse signal-desktop;
+sudo $PKG $INST nvim tmux signal-desktop;
+sudo $PKG $INST tmux --classic;
+sudo $PKG2 $INST calcurse git;
 
